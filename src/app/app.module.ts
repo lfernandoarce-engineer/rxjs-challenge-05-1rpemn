3c0db25b4dccbcaaddbde6ff12e3d2a23f29a1f1
lfernandoarce-engineer <58662558+lfernandoarce-engineer@users.noreply.github.com>
2022-11-30 05:23:52 -0700

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { CountDownComponent } from './count-down/count-down.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, MatCardModule],
   declarations: [AppComponent, CountDownComponent],
   bootstrap: [AppComponent],
 })
