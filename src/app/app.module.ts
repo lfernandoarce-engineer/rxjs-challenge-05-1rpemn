626d3e27e2d301aecd691c120ccf96e930d1abf2
lfernandoarce-engineer <58662558+lfernandoarce-engineer@users.noreply.github.com>
2022-11-30 07:51:42 -0700

    improve styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppComponent } from './app.component';
 import { CountDownComponent } from './count-down/count-down.component';
@@ -15,6 +16,7 @@ import { CountDownComponent } from './count-down/count-down.component';
     MatCardModule,
     MatButtonModule,
     MatIconModule,
+    MatTooltipModule,
   ],
   declarations: [AppComponent, CountDownComponent],
   bootstrap: [AppComponent],
